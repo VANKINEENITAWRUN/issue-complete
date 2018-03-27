7f80dab9a108d36773f029d624e73271a42ee186
Steven Zeck <steven.zeck@gmail.com>
2018-03-26 22:36:46 -0500

    Use LF linebreak style

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import {Robot, Context} from "probot-ts";
-const getConfig = require('probot-config');
+const getConfig = require("probot-config");
 
 const defaultConfig = {
-  labelName: 'waiting-for-user-information',
-  labelColor: 'ffffff',
-  commentText: 'Thanks for opening an issue. I see you haven\'t provided all of the information in the list. Please update the issue to include more information.'
+  labelName: "waiting-for-user-information",
+  labelColor: "ffffff",
+  commentText: "Thanks for opening an issue. I see you haven\"t provided all of the information in the list. Please update the issue to include more information."
 };
 
 const getResource = `
@@ -26,8 +26,8 @@ const addComment = `
 `;
 
 export const robot = (robot: Robot) => {
-  robot.on(['issues.opened', 'issues.edited', 'issues.reopened'], async (context: Context) => {
-    const config = await getConfig(context, 'issuecomplete.yml', defaultConfig);
+  robot.on(["issues.opened", "issues.edited", "issues.reopened"], async (context: Context) => {
+    const config = await getConfig(context, "issuecomplete.yml", defaultConfig);
     validateConfig(context, config);
     const { resource } = await context.github.query(getResource, {
       url: context.payload.issue.html_url
@@ -35,7 +35,7 @@ export const robot = (robot: Robot) => {
     const issueIsIncomplete = validateIssueRequirements(context, config);
     if (issueIsIncomplete) {
       addLabelToIssue(context, config);
-      if (context.payload.action !== 'edited') {
+      if (context.payload.action !== "edited") {
         await context.github.query(addComment, {
           id: resource.id,
           body: config.commentText
@@ -44,16 +44,16 @@ export const robot = (robot: Robot) => {
     } else {
       removeLabelFromIssue(context, config);
     }
-  })
+  });
 
   function validateConfig (context: Context, config: any) {
     const validColor = /^[0-9A-F]{6}$/i.test(config.labelColor);
     if (!validColor) {
-      context.log.error('Invalid color in config file, using default');
+      context.log.error("Invalid color in config file, using default");
       config.labelColor = defaultConfig.labelColor;
     }
     if (config.labelName.length > 50) {
-      context.log.error('Too many characters for label name in config file, using default');
+      context.log.error("Too many characters for label name in config file, using default");
       config.labelName = defaultConfig.labelName;
     }
   }
@@ -63,15 +63,15 @@ export const robot = (robot: Robot) => {
     const hasUncheckedTasks = /-\s\[\s\]/g.test(body);
     let hasMissingKeywords = false;
     if (config.keywords) {
-      for (let i = 0; i < config.keywords.length; i++) {
-        if (body.indexOf(config.keywords[i]) === -1) {
+      for (const i of config.keywords) {
+        if (body.indexOf(i) === -1) {
           hasMissingKeywords = true;
           break;
         }
       }
     }
     if (hasUncheckedTasks || hasMissingKeywords) {
-      context.log('Issue is incomplete, missing checkboxes or keywords', {'Keywords': config.keywords, 'Issue Body': body});
+      context.log("Issue is incomplete, missing checkboxes or keywords", {"Keywords": config.keywords, "Issue Body": body});
       return true;
     }
     return false;
@@ -81,7 +81,7 @@ export const robot = (robot: Robot) => {
     const {owner, repo} = context.repo();
     return context.github.issues.getLabel({owner, repo, name: labelName}).catch(() => {
       return context.github.issues.createLabel({owner, repo, name: labelName, color: labelColor});
-    })
+    });
   }
 
   async function addLabelToIssue (context: Context, config: any): Promise<any> {
@@ -95,6 +95,6 @@ export const robot = (robot: Robot) => {
     const labelRemoval = context.issue({name: labelName});
     return context.github.issues.removeLabel(labelRemoval);
   }
-}
+};
 
 export default robot;
